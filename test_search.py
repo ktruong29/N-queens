669381b728762ad86185298b450c8dcba48e2e99
Benjamin Fuller <benjaminbfuller@csu.fullerton.edu>
2021-12-17 18:30:04 -0800

    Implemented Pygame graphics

--- test_search.py
+++ test_search.py
@@ -1,4 +1,5 @@
 from search import *
+import pygame
 
 '''
 --------------------------------------------------------------
@@ -49,4 +50,71 @@ def test_genetic_algorithm(n):
 
 
 if __name__ == '__main__':
-    print(test_genetic_algorithm(4))
+    print(test_genetic_algorithm(5))
+
+    # * BELOW * Future implementation of automatic 2d array approach for graphics processing
+    # We went with a simpler approach to generate graphics, but has to be configured manually every time
+
+    # testBoard = test_genetic_algorithm(5)  # creates 1d array of queens positions
+
+    pygame.init()
+    tile = 50  # tile size in pixels
+
+    # number_of_rows = len(testBoard)
+    # number_of_columns = len(testBoard)
+    # arr_2d = []
+    # for x in range(number_of_rows):
+    #    column_elements = []
+    #    for y in range(number_of_columns):
+    #        column_elements.append(0)
+    #    arr_2d.append(column_elements)
+    # print(arr_2d)  # creates 2d array
+
+    gameBoard = [ # hard coded 5 queens solution
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 1],
+        [0, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0],
+        [1, 0, 0, 0, 0],
+    ]
+    (width, height) = (len(gameBoard[1]) * tile, len(gameBoard) * tile)
+    window = pygame.display.set_mode((width, height))
+    pygame.display.flip()
+
+
+    def makeBoard():
+        window.fill((0, 0, 0))
+
+        for i in range(len(gameBoard)):
+            for j in range(len(gameBoard[0])):
+                if gameBoard[i][j] == 0:
+                    pygame.draw.rect(window, (255, 255, 255), (j * tile, i * tile, tile, tile))
+                elif gameBoard[i][j] == 1:
+                    pygame.draw.rect(window, (0, 0, 0), (j * tile, i * tile, tile, tile))
+
+        pygame.draw.line(window, (0, 0, 0), (50, 0), (50, 1000))
+        pygame.draw.line(window, (0, 0, 0), (100, 0), (100, 1000))
+        pygame.draw.line(window, (0, 0, 0), (150, 0), (150, 1000))
+        pygame.draw.line(window, (0, 0, 0), (200, 0), (200, 1000))
+        pygame.draw.line(window, (0, 0, 0), (250, 0), (250, 1000))
+        pygame.draw.line(window, (0, 0, 0), (300, 0), (300, 1000))
+
+        pygame.draw.line(window, (0, 0, 0), (0, 50), (1000, 50))
+        pygame.draw.line(window, (0, 0, 0), (0, 100), (1000, 100))
+        pygame.draw.line(window, (0, 0, 0), (0, 150), (1000, 150))
+        pygame.draw.line(window, (0, 0, 0), (0, 200), (1000, 200))
+        pygame.draw.line(window, (0, 0, 0), (0, 250), (1000, 250))
+        pygame.draw.line(window, (0, 0, 0), (0, 300), (1000, 300))
+        pygame.display.update()
+
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    running = False  # quit on key stroke q
+
+        makeBoard()
